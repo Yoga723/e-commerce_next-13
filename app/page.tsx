e32f425e58674af052272d9668584fec8418c94d
Yoga723 <yogapangestu16@gmail.com>
2023-09-02 15:58:25 +0700

    fifth commits

--- app/page.tsx
+++ app/page.tsx
@@ -13,36 +13,36 @@ export default function Home() {
   }
   const { data: session } = useSession(); // Sesi akun ke goggle, dll
   
-  // if (session) {
-  //     const userIMG = session?.user?.image?.toString();
-  //     const userName = session?.user?.name?.toString();
+  if (session) {
+      const userIMG = session?.user?.image?.toString();
+      const userName = session?.user?.name?.toString();
     return (
     <main className="flex h-full">
       <Navigation/>
 
       <div className="flex flex-grow p-3 rounded-lg bg-slate-200 text-black">
-        {/* <p>Hello {userName}!</p> */}
+        <p>Hello {userName}!</p>
       </div>
     </main>
   );
-  // } else {
-  //   return (
-  //     <div className="flex h-full items-center justify-center">
-  //       <button
-  //         type="button"
-  //         className="bg-white p-2 px-4 text-black font-semibold rounded-md"
-  //         onClick={() => signIn("google")}
-  //       >
-  //         Sign In With Google
-  //       </button>
-  //       <button
-  //         type="button"
-  //         className="bg-white p-2 px-4 text-black font-semibold rounded-md"
-  //         onClick={() => signOut()}
-  //       >
-  //         Log Out
-  //       </button>
-  //     </div>
-  //   );
-  // }
+  } else {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <button
+          type="button"
+          className="bg-white p-2 px-4 text-black font-semibold rounded-md"
+          onClick={() => signIn("google")}
+        >
+          Sign In With Google
+        </button>
+        <button
+          type="button"
+          className="bg-white p-2 px-4 text-black font-semibold rounded-md"
+          onClick={() => signOut()}
+        >
+          Log Out
+        </button>
+      </div>
+    );
+  }
 }
